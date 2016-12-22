55ad5e8bb6cd82a5c4e394cdb0b9a5cbc3f1391f
Rhett Garber <rhettg@github.com>
2016-12-21 17:03:56 -0800

    allow ping

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+  from_port = 8
+  to_port = 0
+  protocol = "icmp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
   egress {
     from_port   = 0
     to_port     = 0
